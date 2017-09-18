651509d91018bbf011ce200dab4912acf98d314d
leahliya <leahliya@gmail.com>
2017-09-18 09:19:31 -0500

    pushing

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ angular.module('weatherApp', ['ui.router'])
     $stateProvider
          .state('home',{
              url:'/',
-             templateUrl: "../views/about.html",
+             templateUrl: "/views/about.html",
              controller: "aboutCtrl"
          })
          .state('weather',{
              url:'/weather/:name',
-             templateUrl: "../views/weather.html",
+             templateUrl: "/views/weather.html",
              controller: "weatherCtrl"
          })
          .state('fiveDaysForecast',{
              url:'/fiveDaysForecast/:location',
-             templateUrl: "../views/fiveDay.html",
+             templateUrl: "/views/fiveDay.html",
              controller: "fiveDayCtrl"
          })
          .state('contact',{
             url:'/contact',
-            templateUrl: "../views/contact.html"
+            templateUrl: "/views/contact.html"
         })     
   
       $urlRouterProvider
